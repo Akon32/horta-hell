ec74323a082f6f2dcec11986ccc2d13a818b1a87
Alexander Konovalov <akon32@rambler.ru>
2014-03-08 11:53:09 +0400

    Extracted 5 plugins:
      access-plugin
      fortune-plugin
      pet-plugin
      bash-plugin
      markov-plugin

--- project/HortaBuild.scala
+++ project/HortaBuild.scala
@@ -6,7 +6,24 @@ object HortaBuild extends Build {
 
   lazy val `horta-core` = project in file("horta-core")
 
-  lazy val plugins = project in file("plugins") dependsOn versionPlugin
+  lazy val plugins = project in file("plugins") dependsOn (
+    versionPlugin,
+    petPlugin,
+    markovPlugin,
+    bashPlugin,
+    fortunePlugin,
+    accessPlugin)
 
   lazy val versionPlugin = project in file("plugins/version") dependsOn `horta-core`
+
+  lazy val petPlugin = project in file("plugins/pet") dependsOn `horta-core`
+
+  lazy val markovPlugin = project in file("plugins/markov") dependsOn `horta-core`
+
+  lazy val bashPlugin = project in file("plugins/bash") dependsOn `horta-core`
+
+  lazy val fortunePlugin = project in file("plugins/fortune") dependsOn `horta-core`
+
+  lazy val accessPlugin = project in file("plugins/access") dependsOn `horta-core`
+
 }
